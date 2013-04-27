753eb575a2e0fa14b021477c46c591829eaa386d
Pat Goebel <putterson@gmail.com>
2013-04-27 17:22:19 -0400

    seperated canvas version and svg version

--- theory.js
+++ theory.js
@@ -1,178 +1,178 @@
-function drawBoard(){
+function svgBoard(){
+	var xmlns="http://www.w3.org/2000/svg";
+	
+	//svg meta elements
+	var container;
+	var svg;
+	var g_fret;
+	var g_note;
+	
+	//svg drawing elements
+	var strings;
+	var frets;
+	var fretnums;
+	var nut;
+	
+	
+	//params
 	var nfrets = 15;
 	var nstrings = 6;
-	
-	var canvas_fret = document.getElementById("fretBoard");
-	var canvas_note = document.getElementById("noteBoard");
-	var ctx_fret = canvas_fret.getContext("2d");
-	var ctx_note = canvas_note.getContext("2d");
-	
-	//These are applicable to both the noteBoard and fretBoard as they should be the same size
-	var cwidth  = canvas_fret.width;
-	var cheight = canvas_fret.height;
-	var lpad = canvas_fret.width * .05;
-	var rpad = canvas_fret.width * .05;
-	var tpad = canvas_fret.height* .20;
-	var bpad = canvas_fret.height* .10;
-	var bwidth  = cwidth  - lpad - rpad;
-	var bheight = cheight - tpad - bpad;
-	
-	//Some canvas drawing parameters
-	var c_stroke = "#555555";
-	var c_fill = "#555555";
-	var c_marker = "#999999";
-	var c_markerstroke = "#FFFFFF";
-	var c_string = "#111111";
-	var c_note = "#FFFFFF";
-	var c_note_active = "#73E6BF";
-	var c_note_selected = "#F385F3";
-	var c_notestroke = "#000000";
-	var c_text = "#000000";
-	var strokewidth = 1;
 	var stringwidth = 0.4;
 	
-	//Music Theory
-	var a_tuning =["E","A","D","G","B","E"];
-	var a_notes = new Array("A","A♯","B","C","C♯","D","D♯","E","F","F♯","G","G♯");
-	var a_Em_pentatonic = ["E","G","A","B","D"]
+	//These are applicable to both the noteBoard and fretBoard as they should be the same size
+	var cwidth;
+	var cheight;
+	var lpad;
+	var rpad;
+	var tpad;
+	var bpad;
+	var bwidth;
+	var bheight;
 	
-	function align(x){
+	function alignx(x){
+			return x - (x % 1);
+	}
+	this.alignon = alignx;
+	
+	function aligny(x){
 			return x - (x % 1) + 0.5;
 	}
+	this.alignin = aligny;
 	
-	//draw the note names
-	function draw_notes(){
-		var n_start = 0;
-		var n_note = 0;
-		var x = 0;
-		var y = 0;
-		for(var s=nstrings-1; s >= 0; s--){
-			start = a_notes.indexOf(a_tuning[a_tuning.length - s - 1]);
-			y = align((s*(bheight/(nstrings - 1))) + tpad);
-			
-			//handle fret 0 seperately
-			x = align((0*(bwidth/nfrets)) + lpad);
-			
+	
+	//populate all persistent objects
+	function construct(){
+		//create initial svg elem
+		container = document.getElementById("svgContainer");
+		svg = document.createElementNS(xmlns, "svg");
+		svg.setAttribute("version", "1.2");
+		svg.setAttribute("baseProfile", "tiny");
+		svg.setAttribute("id","svgBoard");
+		
+		resize_params();
+		
+		container.appendChild(svg);
+		
+		//create fret and note svg groups
+		g_note = document.createElementNS(xmlns, "g");
+		g_fret = document.createElementNS(xmlns, "g");
+		
+		svg.appendChild(g_note);
+		svg.appendChild(g_fret);
+		
+		create_strings();
+		create_frets();
+		
+	}
+	
+	function resize_params(){
+		svg.setAttribute("height",window.innerHeight * 0.20);
+		svg.setAttribute("width",window.innerWidth);
+		cwidth  = svg.getAttribute("width");
+		cheight = svg.getAttribute("height");
+		lpad = 25;//cwidth * .05;
+		rpad = 25;//cwidth * .05;
+		tpad = cheight* .20;
+		bpad = cheight* .10;
+		bwidth  = cwidth  - lpad - rpad;
+		bheight = cheight - tpad - bpad;
+	}
 
-			
-			for(var f=0; f <= nfrets; f++){
-				x = align((f*(bwidth/nfrets)) + lpad);
-				if (f==0){ x -= 6; }
-				else { x -= 20; }
-				
-				if (a_Em_pentatonic.indexOf(a_notes[(start + f) % 12]) >= 0){
-					ctx_note.fillStyle = c_note_selected;
-				}else {
-					ctx_note.fillStyle = c_note_active;
-				}
-
-				ctx_note.strokeStyle = c_notestroke;
-				
-				ctx_note.beginPath();
-				ctx_note.arc(x+6,y, 9, 0, 2*Math.PI);
-				ctx_note.closePath();
-				ctx_note.stroke();
-				ctx_note.fill();
-				
-				ctx_note.fillStyle = c_text;
-				ctx_note.strokeStyle = c_text;
-				
-				ctx_note.fillText(a_notes[(start + f) % 12], x, y + 4);
-			}
+	function create_strings(){
+		strings = new Array();
+		var line;
+		for(var i=0; i < nstrings; i++){
+			line = document.createElementNS(xmlns, "line");
+			line.setAttribute("id","string");
+			strings[i] = line;
+			g_fret.appendChild(line);
 		}
 	}
 	
-	//draw a fretmarker
-	function fretmarker(x,y){
-		x = align(x);
-		y = align(y);
+	function create_frets(){
+		frets = new Array();
 		
-		ctx_fret.fillStyle = c_marker;
-		ctx_fret.strokeStyle = c_markerstroke;
+		//create nut seperately
+		var rect;
+		rect = document.createElementNS(xmlns, "rect");
+		nut = rect;
+		g_fret.appendChild(rect);
 		
-		var size = bheight * 0.05;
-		ctx_fret.beginPath();
-		ctx_fret.arc( x, y, size, 0, 2*Math.PI);
-		ctx_fret.closePath();
-		ctx_fret.fill();
+		var line;
+		var x;
 		
-		ctx_fret.fillStyle = c_fill;
-		ctx_fret.strokeStyle = c_stroke;
-	}
-	
-	//draw the string lines~
-	function draw_strings(){
-		ctx_fret.strokeStyle = c_string;
-		for (var x=0; x<nstrings; x++)
+		for (var i=0; i<=nfrets; i++)
 		{
-			ctx_fret.lineWidth = 1 + (x*stringwidth);
-			ctx_fret.beginPath();
-			ctx_fret.moveTo( align(lpad) , align((x*(bheight/(nstrings - 1))) + tpad) );
-			ctx_fret.lineTo( align(lpad + bwidth) , align((x*(bheight/(nstrings - 1))) + tpad) );
-			ctx_fret.closePath();
-			ctx_fret.stroke();
-		}
-		ctx_fret.linewidth = strokewidth;
-		ctx_fret.strokeStyle = c_stroke;
-	}
-	
-	//draw the fret lines and numbers and markers
-	function draw_frets(){
-		var x=0;
-		for (var i=x; i<(nfrets + 1); i++)
-		{
-			x = align((i*(bwidth/nfrets)) + lpad);
-
-			if (i > 1 && i % 2 == 1 && (i+1) % 12 != 0 && (i-1) % 12 != 0){
-				fretmarker(x - (bwidth/nfrets/2), tpad + (bheight/2))
-			} else if (i > 1 && i % 12 == 0){
-				fretmarker(x - (bwidth/nfrets/2), tpad + (bheight*(1/3)));
-				fretmarker(x - (bwidth/nfrets/2), tpad + (bheight*(2/3)));
-			}
-			ctx_fret.beginPath();
-			ctx_fret.moveTo( x , align(tpad) );
-			ctx_fret.lineTo( x , align(tpad + bheight) );
-			ctx_fret.closePath();
-			ctx_fret.stroke();
-			
-			ctx_fret.fillText(i, x - 4, align(tpad) - 15);
+			line = document.createElementNS(xmlns, "line");
+			frets[i] = line;
+			g_fret.appendChild(line);
 		}
 	}
-
-	//draw the nut
-	ctx_fret.fillStyle = c_fill;
-	ctx_fret.fillRect(lpad-3 , tpad - 5 , 3, bheight + 10);
 	
-	ctx_fret.strokeStyle = c_stroke;
-	ctx_fret.font="10px Monospace";
+	function resize_strings(){
+		for(var x=0; x < nstrings; x++){
+			strings[x].setAttribute("x1", alignx(lpad) );
+			strings[x].setAttribute("y1", aligny(x*(bheight/(nstrings - 1)) + tpad) );
+			strings[x].setAttribute("x2", alignx(lpad + bwidth) );
+			strings[x].setAttribute("y2", aligny(x*(bheight/(nstrings - 1)) + tpad) );
+			strings[x].setAttribute("style", "stroke-width: " + (1 + (x*stringwidth)) + "px;" + "stroke: black;");
+		}
+	}
 	
-	ctx_note.font="12px Sans Serif";
+	function resize_frets(){
+		for (var i=0; i<= nfrets; i++){
+			x = aligny((i*(bwidth/nfrets)) + lpad);
+			frets[i].setAttribute("x1", x);
+			frets[i].setAttribute("y1", aligny(tpad));
+			frets[i].setAttribute("x2", x);
+			frets[i].setAttribute("y2", aligny(tpad + bheight));
+			frets[i].setAttribute("style", "stroke-width: 1px; stroke: black;");
+		}
+	}
 	
-	draw_frets();
-	draw_strings();
-	draw_notes();
-
+	this.resize = function(){
+		resize_params();
+		resize_strings();
+		resize_frets();
+	}
+	
+	construct();
+	this.resize();
 }
 
 function resize_board(id){
-	canvas = document.getElementById(id);
-	if (canvas.width  != window.innerWidth)
+	var canvas = document.getElementById(id);
+	if (canvas.width != window.innerWidth)
 	{
-		canvas.width  = window.innerWidth;
+		canvas.width = window.innerWidth;
+		canvas.setAttribute("width",window.innerWidth);
 	}
 
 	if (canvas.height != window.innerHeight)
 	{
 		if(window.innerHeight * .20 < 150){
 			canvas.height = 150;
+			canvas.setAttribute("height", 150);
 		} else {
 			canvas.height = window.innerHeight * .20;
+			canvas.setAttribute("height", window.innerHeight * .20);
 		}
 	}
+}
+
+function load_theory(){
+	this.svgboard = new svgBoard();
+// 	this.board = new Board("fretBoard","noteBoard");
+	resize_theory();
 }
 
 function resize_theory(){
 	resize_board("fretBoard");
 	resize_board("noteBoard");
-	drawBoard();
+	resize_board("svgBoard");
+	svgboard.resize();
+// 	board.draw();
 }
+
+window.onload = load_theory;
+window.onresize = resize_theory;

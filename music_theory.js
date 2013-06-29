936026b2043873d997f30ae4e8e266df37f5a4d3
Pat Goebel <putterson@gmail.com>
2013-06-29 02:53:00 -0400

    started adding music theory

---
+++ music_theory.js
@@ -0,0 +1,14 @@
+function musicTheory(){
+	var tunings = {
+		"Standard" : ["E","A","D","G","B","E"],
+		"Drop D"   : ["D","A","D","G","B","E"],
+	}
+	var tones = {
+		"Sharps" : ["A","A♯","B","C","C♯","D","D♯","E","F","F♯","G","G♯"],
+		"Flats"  : ["A","B♭","B","C","D♭","D","E♭","E","F","G♭","G","A♭"]
+		
+	}
+	var scales = {
+		"pentatonic_Em" : ["E","G","A","B","D"]
+	}
+}

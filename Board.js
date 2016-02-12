d88a02f617f790ee9c9462f07b9609b890bd7326
Pat Goebel <putterson@gmail.com>
2016-02-11 22:46:48 -0500

    Add switching between tunings

--- Board.js
+++ Board.js
@@ -1,3 +1,47 @@
+var tunings = [
+    { name: "Standard",
+      strings: [{ note: "E",
+		  octave: 4 },
+		{ note: "B",
+		  octave: 3 },
+		{ note: "G",
+		  octave: 3 },
+		{ note: "D",
+		  octave: 3 },
+		{ note: "A",
+		  octave: 2 },
+		{ note: "E",
+		  octave: 2 }]},
+    { name: "Drop D",
+      strings: [{ note: "E",
+		  octave: 4 },
+		{ note: "B",
+		  octave: 3 },
+		{ note: "G",
+		  octave: 3 },
+		{ note: "D",
+		  octave: 3 },
+		{ note: "A",
+		  octave: 2 },
+		{ note: "D",
+		  octave: 2 }]},
+    { name: "7-String",
+      strings: [{ note: "E",
+		  octave: 4 },
+		{ note: "B",
+		  octave: 3 },
+		{ note: "G",
+		  octave: 3 },
+		{ note: "D",
+		  octave: 3 },
+		{ note: "A",
+		  octave: 2 },
+		{ note: "E",
+		  octave: 2 },
+		{ note: "B",
+		  octave: 2 }]}
+]
+
 export default {
     getFrets(n) {
 	var frets = []
@@ -5,5 +49,21 @@ export default {
 	    frets.push({n: i})
 	}
 	return frets
+    },
+
+    getDefaultTuning() {
+	return this.getTuning("Standard")
+    },
+
+    getStrings(tuning) {
+	return tunings.filter( (value) => value.name === tuning.name )[0].strings
+    },
+
+    getTunings() {
+	return tunings
+    },
+
+    getTuning(tuningName) {
+	return tunings.filter( (value) => value.name === tuningName )[0]
     }
 }
--- app.js
+++ app.js
@@ -8,56 +8,57 @@ import Board from "./Board"
 
 let App = React.createClass({
     getInitialState() {
+	var default_tuning = Board.getDefaultTuning()
 	return {
-	    value: "",
-	    strings: [{ note: "E",
-			octave: 4 },
-		      { note: "B",
-			octave: 3 },
-		      { note: "G",
-			octave: 3 },
-		      { note: "D",
-			octave: 3 },
-		      { note: "A",
-			octave: 2 },
-		      { note: "E",
-			octave: 2 }],
+	    tuning: default_tuning,
+	    strings: Board.getStrings(default_tuning),
 	    frets: Board.getFrets(12)
-	    };
-	},
+	};
+    },
 
-	componentDidMount() {
-	    window.addEventListener('resize', this.handleResize);
-	},
+    componentDidMount() {
+	window.addEventListener('resize', this.handleResize);
+    },
 
-	handleResize() {
-	    this.setAppState(this.state);
-	},
+    handleResize() {
+	this.setAppState(this.state);
+    },
 
-	setAppState(partialState, callback) {
-	    return this.setState(partialState, callback);
-	},
+    setAppState(partialState, callback) {
+	return this.setState(partialState, callback);
+    },
+
+    setTuning(tuning) {
+	this.state.tuning = tuning
+	this.state.strings = Board.getStrings(tuning)
+	this.setState(this.state)
+    },
 
 
-	render() {
-	    return <div> 
-		<div> 
-		{"Value is " + this.state.value + "!"}
-            </div>
-		<div> 
-		<input 
-            type="text" 
-            key="myUniqueComponentIdentifier"
-            onChange={({target: {value}}) => {
-		this.state.value = value;
-		this.setState(this.state);
-            }} /> 
-		</div>
-		<Fretboard
-            appState={this.state}
-            setAppState={this.setAppState} />
-		</div>;
-	}
-    });
-			    ReactDOM.render(<App/>, document.getElementById("app-container"));
+    render() {
+	return <div> 
+	    <div> 
+	    {"Tuning is " + this.state.tuning.name}
+        </div>
+	    <div> 
+	    <select value={this.state.tuning.name}
+	onChange={({target: {value}}) => {
+	    this.setTuning(Board.getTuning(value))
+        }}
+	    >
+	    {
+		Board.getTunings().map(function(tuning) {
+		    return <option key={tuning.name}
+		    value={tuning.name}>{tuning.name}</option>;
+		})
+	    }
+	</select>
+	    </div>
+	    <Fretboard
+        appState={this.state}
+        setAppState={this.setAppState} />
+	    </div>;
+    }
+});
+ReactDOM.render(<App/>, document.getElementById("app-container"));
 

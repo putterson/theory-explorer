5624019be752bfd8c28ee40e9c19fe81e5a92395
Pat Goebel <putterson@gmail.com>
2016-02-29 20:50:15 -0500

    Replace makefile and run scripts with npm scripts

--- watch.sh
+++
@@ -1,5 +0,0 @@
-#put electron on the path if it is not installed globally
-export PATH="`pwd`/node_modules/.bin/:$PATH"
-
-ps -x | grep watchify | grep -v grep | cut -d" " -f2 | xargs kill
-watchify -t [ babelify --presets [ es2015 react ] ] app.js -o bundle.js --debug &

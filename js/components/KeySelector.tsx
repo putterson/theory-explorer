4c70ba6224ba4baf454fb81aea1241d322924df1
Pat Goebel <putterson@gmail.com>
2016-04-04 22:06:10 -0400

    Fix selected key button class

--- js/components/KeySelector.tsx
+++ js/components/KeySelector.tsx
@@ -1,14 +1,20 @@
 const React = require('react')
+import {PitchClass} from '../stores/Board'
 
-const KeySelector = ({ onKeySelect, keys, currentKey }) => (
+interface KeySelectorState {
+    onKeySelect : Function,
+    keys : Array<PitchClass>,
+    currentKey : PitchClass
+}
+
+const KeySelector = ({ onKeySelect, keys, currentKey } : KeySelectorState) => (
     <div className="btn-group" role="group" data-toggle="buttons">
-    {keys.map(function(key){
+    {keys.map(function(key : PitchClass){
       return <button
       type="button"
-      className={"btn btn-default" + (key.name === currentKey ? 'active' : '')}
+      className={"btn btn-default" + (key.name === currentKey.name ? 'active' : '')}
       key={key.id}
       id={key.id}
-      aria-pressed={key.name === currentKey}
       onClick={onKeySelect.bind(this, key.id)}>
 	{key.name}
 	</button>
